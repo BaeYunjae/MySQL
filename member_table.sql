3ae7a971ba7b4d1a2f723c75abb01709c69874a4
BaeYunjae <qodbswo1010@gmail.com>
2024-02-08 12:04:14 +0900

    MySQL

--- member_table.sql
+++ member_table.sql
@@ -1,4 +1,4 @@
-1. 모든 필드 출력 + member_id가 3, 5번 레코드만 출력하기
+-- 1. 모든 필드 출력 + member_id가 3, 5번 레코드만 출력하기
     
 SELECT * FROM member WHERE member_id=3 OR member_id=5;
 +-----------+--------+-----+
@@ -8,7 +8,8 @@ SELECT * FROM member WHERE member_id=3 OR member_id=5;
 |         5 | 현철   |  18 |
 +-----------+--------+-----+
     
-2. name, age 필드 출력하고, 2 <= member_id <= 5번 레코드만 출력하기 (조건에 AND 사용)
+
+-- 2. name, age 필드 출력하고, 2 <= member_id <= 5번 레코드만 출력하기 (조건에 AND 사용)
     
 SELECT name, age FROM member WHERE member_id>=2 AND member_id<=5;
 +--------+-----+
@@ -20,7 +21,8 @@ SELECT name, age FROM member WHERE member_id>=2 AND member_id<=5;
 | 현철   |  18 |
 +--------+-----+
     
-3. 나이가 20대인 친구들만 출력 (모든 필드 출력)
+
+-- 3. 나이가 20대인 친구들만 출력 (모든 필드 출력)
     
 SELECT * FROM member WHERE age>=20 AND age<30;
 +-----------+--------+-----+
@@ -32,7 +34,7 @@ SELECT * FROM member WHERE age>=20 AND age<30;
 +-----------+--------+-----+
 
     
-4. member_id가 짝수이면서, age는 홀수인 친구들만 출력 (사람 이름만 출력)
+-- 4. member_id가 짝수이면서, age는 홀수인 친구들만 출력 (사람 이름만 출력)
     
 SELECT name FROM member WHERE member_id%2=0 AND age%2=1;
 +--------+
@@ -42,7 +44,8 @@ SELECT name FROM member WHERE member_id%2=0 AND age%2=1;
 | 장현   |
 +--------+
     
-5. member_id를 “번호”, name을 “성함”, age를 “나이” 로 출력 + 나이 오름차순으로 정렬
+
+-- 5. member_id를 “번호”, name을 “성함”, age를 “나이” 로 출력 + 나이 오름차순으로 정렬
     
 SELECT member_id AS '번호', name AS '성함', age AS '나이' FROM member ORDER BY age;
 +--------+-----------+--------+
@@ -58,7 +61,7 @@ SELECT member_id AS '번호', name AS '성함', age AS '나이' FROM member ORDE
 +--------+-----------+--------+
 
     
-6. 이름 사전 순서대로(오름차순) 나이, 이름만 출력하기
+-- 6. 이름 사전 순서대로(오름차순) 나이, 이름만 출력하기
     
 SELECT age,name FROM member ORDER BY name;
 +-----+-----------+
@@ -73,7 +76,8 @@ SELECT age,name FROM member ORDER BY name;
 |  18 | 현철      |
 +-----+-----------+
     
-7. member_id 2 번과 4 번 레코드의 나이를 모두 15로 수정
+
+-- 7. member_id 2 번과 4 번 레코드의 나이를 모두 15로 수정
     
 mysql> UPDATE member SET age=15 WHERE member_id=2 OR member_id=4;
 Query OK, 2 rows affected (0.00 sec)
@@ -93,7 +97,7 @@ mysql> SELECT * FROM member;
 +-----------+-----------+-----+
 
     
-8. 1 번 레코드 삭제
+-- 8. 1 번 레코드 삭제
 
 mysql> DELETE FROM member WHERE member_id=1;
 Query OK, 1 row affected (0.01 sec)
@@ -109,8 +113,9 @@ mysql> SELECT * FROM member;
 |         6 | 장현      |  29 |
 |         8 | 박중박    |  50 |
 +-----------+-----------+-----+
-    
-9. 레코드 추가 (촉촉, 100)
+
+
+-- 9. 레코드 추가 (촉촉, 100)
 
 INSERT INTO member (`name`, `age`) VALUES ('촉촉', 100);
 Query OK, 1 row affected (0.01 sec)
